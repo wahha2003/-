379b9c8565dd505a2d69be3a49356c4c25817bfd
wahha2003 <104408266+wahha2003@users.noreply.github.com>
2024-10-10 19:19:08 +0800

    Create kousuan.js

---
+++ kousuan.js
@@ -0,0 +1,39 @@
+/*
+Quantumult X Rewrite Script
+[rewrite_local]
+# 拦截URL及其参数的POST请求
+^https:\/\/xyks\.yuanfudao\.com\/leo-game-pk\/iphone\/math\/pk\/match.*$ script-response-body myscript.js
+
+[MITM]
+hostname = xyks.yuanfudao.com
+*/
+
+// 获取服务器响应的原始数据
+let body = $response.body;
+
+// 将响应内容格式化为JSON对象
+let jsonBody;
+try {
+    jsonBody = JSON.parse(body);
+} catch (e) {
+    console.log("Error parsing JSON: " + e);
+    $done({body});
+}
+
+// 提取每个题目的答案
+if (jsonBody && jsonBody.examVO && jsonBody.examVO.questions) {
+    let questions = jsonBody.examVO.questions;
+    
+    // 构建答案列表的输出
+    let answerMessage = questions.map((q, index) => {
+        return (index + 1) + ". [" + q.answers[0] + "]";
+    }).join(" ");
+    
+    // 发送弹窗，展示答案
+    $notify("题目答案", "", answerMessage);
+    
+    console.log("Answers: " + answerMessage);  // 也可以在日志中输出
+}
+
+// 返回原始响应内容
+$done({body});
