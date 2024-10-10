96a967ef1fe0dcae9456bfcfea45c76fde862e41
wahha2003 <104408266+wahha2003@users.noreply.github.com>
2024-10-10 23:00:36 +0800

    Update kousuan.js

--- kousuan.js
+++ kousuan.js
@@ -1,7 +1,6 @@
-// 获取服务器响应的原始数据
 let body = $response.body;
 
-// 将响应内容格式化为JSON对象
+// 将响应内容格式化为 JSON 对象
 let jsonBody;
 try {
     jsonBody = JSON.parse(body);
@@ -14,15 +13,24 @@ try {
 if (jsonBody && jsonBody.examVO && jsonBody.examVO.questions) {
     let questions = jsonBody.examVO.questions;
     
-    // 构建答案列表的输出
-    let answerMessage = questions.map((q, index) => {
-        return (index + 1) + ". [" + q.answers[0] + "]";
-    }).join(" ");
+    // 构建答案列表，按批次分10个题目一组
+    let answerMessages = [];
+    let batchSize = 10;
     
-    // 5 秒后发送弹窗，展示答案
-    setTimeout(() => {
-        $notify("题目答案", "", answerMessage);
-    }, 8000);  // 延迟5秒 (5000毫秒)
+    // 分批次处理答案
+    for (let i = 0; i < questions.length; i += batchSize) {
+        let batch = questions.slice(i, i + batchSize).map((q, index) => {
+            return (i + index + 1) + ". [" + q.answers[0] + "]";
+        }).join(" ");
+        answerMessages.push(batch);
+    }
+
+    // 按批次延时展示答案，每批间隔8秒
+    answerMessages.forEach((msg, index) => {
+        setTimeout(() => {
+            $notify("题目答案", "第 " + (index + 1) + " 批次", msg);
+        }, 8000 * (index + 1));  // 每批次间隔 8 秒
+    });
 }
 
 // 返回原始响应内容
